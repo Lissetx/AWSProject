d5ca23c5cbee725b32968bbe77a36c6a862b1558
Lissetx <98356241+Lissetx@users.noreply.github.com>
2023-03-24 12:27:33 -0600

    git updates

--- PerformerService/PerformerDelete/ECR_PUSH.bat
+++
@@ -1,9 +0,0 @@
-::.f connection is valid with login acess
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 705517780837.dkr.ecr.us-west-1.amazonaws.com/auditonme
-
-::Docker fun stuff
-docker compose up -d
-
-::tags and pushes to ECR
-docker tag performer_delete:1.0 705517780837.dkr.ecr.us-west-1.amazonaws.com/auditonme:performer_delete
-docker push 705517780837.dkr.ecr.us-west-1.amazonaws.com/auditonme:performer_delete
